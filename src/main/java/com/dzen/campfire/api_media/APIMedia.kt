db2c0ef3504a23b71c9ec952ff1bb8d8ef46f72e
Холин Дмитрий <kholin@beward.ru>
2019-03-14 15:30:10 +0700

    Commit

--- src/main/java/com/dzen/campfire/api_media/APIMedia.kt
+++ src/main/java/com/dzen/campfire/api_media/APIMedia.kt
@@ -1,22 +1,21 @@
 package com.dzen.campfire.api_media
 
-import com.dzen.campfire.api.tools.client.ApiClient
-import com.dzen.campfire.api.tools.client.TokenProvider
+import com.sup.dev.java.libs.api_simple.client.ApiClient
+import com.sup.dev.java.libs.api_simple.client.TokenProvider
+import java.io.File
 
 class APIMedia(
-        projectKey:String,
         tokenProvider: TokenProvider,
         host: String,
         portHttps: Int,
         portCertificate: Int,
         saver: (String, String?) -> Unit,
         loader: (String) -> String?
-) : ApiClient(projectKey, tokenProvider, host, portHttps, portCertificate, saver, loader) {
+) : ApiClient(tokenProvider, host, portHttps, portCertificate, saver, loader) {
 
     companion object {
 
         val PORT_HTTPS = 4023
-        val PORT_HTTP = 4022
         val PORT_CERTIFICATE = 4024
         val IP = "46.254.16.245"
         val VERSION = "1"
